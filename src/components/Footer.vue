22e220d6e78b7834d6cc5521327de2758cc49810
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-10-10 16:02:49 +0200

    [add] Footer and fontawesome

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,28 @@
 <script>
-export default {
+  export default {
     name: 'Footer',
-}
+};
 </script>
-
-<template>
   
+<template>
+    <footer>
+        <div class="container row">
+            <nav class="copyright">
+                <h5 class="name-company">Booleando s.r.l</h5>
+                <ul>
+                    <li><a href="#">Informazioni legali</a></li>
+                    <li><a href="#">Informativa sulla privacy</a></li>
+                    <li><a href="#">Diritto di recesso</a></li>
+                </ul>
+            </nav>
+        </div>
+        <nav class="social">
+            <h5>Trovaci anche su</h5>
+            <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
+            <a href="#"><i class="fa-brands fa-square-youtube"></i></a>
+        </nav>
+    </footer>
 </template>
-
-<style>
-
-</style>
